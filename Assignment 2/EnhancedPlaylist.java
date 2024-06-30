9efc489ba4f543dcbe79b6a446f03d58a8e04c2f
nanadotam <nanaamoako202@gmail.com>
2024-06-30 14:08:16 +0000

    Changes made to EnhancedPlaylist

--- Assignment 2/EnhancedPlaylist.java
+++ Assignment 2/EnhancedPlaylist.java
@@ -1,3 +1,41 @@
 public class EnhancedPlaylist {
-    
+    /*
+     * Node class
+     * @param song Song object
+     * @param prev Node object
+     * @param next Node object
+     */
+    private class Node { 
+        Song song;
+        Node prev;
+        Node next;
+
+    /*
+         * Node constructor
+         * @param song Song object
+         */
+        // Constructor
+        Node (Song song) {
+            this.song = song;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    /*
+     * Head of the playlist
+     * @param head Node
+     */
+    private Node head;
+
+    /* 
+     * BasicPlaylist constructor
+     * @param head Node
+     */
+    // Constructor
+    public BasicPlaylist() {
+        this.head = null;
+    }
+
+
 }
