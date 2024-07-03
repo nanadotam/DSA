105592d631f5006d7cc52fc9e51d3e5fede8ae20
nanaamoako202 <nanaamoako202@gmail.com>
2024-07-03 11:55:05 +0000

    Started FullyFunctionalPlaylist

--- Assignment 2/EnhancedPlaylist.java
+++ Assignment 2/EnhancedPlaylist.java
@@ -10,7 +10,7 @@ public class EnhancedPlaylist {
         Node prev;
         Node next;
 
-    /*
+        /*
          * Node constructor
          * @param song Song object
          */
--- Assignment 2/FullyFunctionalPlaylist.java
+++ Assignment 2/FullyFunctionalPlaylist.java
@@ -1,3 +1,18 @@
 public class FullyFunctionalPlaylist {
+    private class Node {
+        Song song;
+        Node prev;
+        Node next;
+    
+        // Constructor
+        Node (Song song) {
+            this.song = song;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+
+
     
 }
