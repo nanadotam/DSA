0f45d13669a038903031a738ba2f75e4d48e37eb
nanadotam <nanaamoako202@gmail.com>
2024-07-07 05:36:56 +0000

    Code Clean Up and Search method

--- Assignment 2/FullyFunctionalPlaylist.java
+++ Assignment 2/FullyFunctionalPlaylist.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FullyFunctionalPlaylist {
-    /*
+    /**
      * Node class
      * @param song Song object
      * @param prev Node object
@@ -11,7 +14,7 @@ public class FullyFunctionalPlaylist {
         Node next;
         
 
-        /*
+        /**
          * Node constructor
          * @param song Song object
          */
@@ -22,7 +25,7 @@ public class FullyFunctionalPlaylist {
         }
     }
 
-    /*
+    /**
      * Head of the playlist
      * Tail of the playlist
      * @param head Node
@@ -32,7 +35,7 @@ public class FullyFunctionalPlaylist {
     private Node tail;
     private boolean repeatAll; // continuous play
 
-    /* 
+    /**
      * FullyFunctionalPlaylist constructor
      * @param head Pointer for traversing through list.
      */
@@ -41,7 +44,7 @@ public class FullyFunctionalPlaylist {
         this.tail = null;
     }
 
-    /*
+    /**
      * Adds a song to the end of the playlist.
      * @param song Song to be added
      */
@@ -61,7 +64,7 @@ public class FullyFunctionalPlaylist {
     }
 
 
-    /*
+    /**
      * Adds a song to the playlist at a specified position.
      * @param song Song to be added.
      * @param position Position to add the song at
@@ -104,7 +107,7 @@ public class FullyFunctionalPlaylist {
         }
     }
 
-    /*
+    /**
     * Removes a song from the playlist using its Title
     * @param title Title of song to be removed
     */ 
@@ -140,7 +143,7 @@ public class FullyFunctionalPlaylist {
         }
     }
 
-    /*
+    /**
      * Removes a song at a specified position
      * @param position Position of song to be removed
      */
@@ -170,7 +173,7 @@ public class FullyFunctionalPlaylist {
     }
 
 
-    /*
+    /**
     * Prints the playlist
     */
     public void displayPlaylist() {
@@ -190,64 +193,7 @@ public class FullyFunctionalPlaylist {
         }
     }
 
-    // /*
-    // * Prints the playlist
-    // */
-    // public void displayPlaylist() {
-    //     if (head == null) {
-    //         return; // if the list is empty, do nothing
-    //     }
-    //     Node current = head;
-    //     do {
-    //         System.out.println("Title: " + current.song.title + ", Artist: " + current.song.artist + ", Duration: " + current.song.getFormattedDuration());
-    //         current = current.next;
-    //     } while (current != head); // continue until we return to the head
-    // }
-
-    // /*
-    // * Prints the playlist
-    // */
-    // public void displayPlaylist() {
-    //     if (head == null) {
-    //         return; // if the list is empty, do nothing
-    //     }
-    //     Node current = head;
-    //     while (true) {
-    //         System.out.println("Title: " + current.song.title + ", Artist: " + current.song.artist + ", Duration: " + current.song.getFormattedDuration());
-    //         current = current.next;
-    //         if (current == head) {
-    //             break; // traversed the whole list and returned to the head
-    //         }
-    //     }
-        
-    // }
-
-    // /*
-    //  * Prints the playlist
-    //  */
-    // public void displayPlaylist() {
-    //     if (head == null) return; // if the list is empty, do nothing
-    //     Node current = head;
-    //     do {
-    //         System.out.println("Title: " + current.song.title + ", Artist: " + current.song.artist + ", Duration: " + current.song.getFormattedDuration());
-    //         current = current.next;
-    //     } while (current != head);
-    // }
-
-    // /*
-    //  * Prints the playlist
-    //  */
-    // public void displayPlaylist() {
-    //     Node current = head;
-    //     while (current != null) {
-    //         // System.out.println(current.song);
-    //         System.out.println("Title: " + current.song.title + ", Artist: " + current.song.artist + ", Duration: " + current.song.getFormattedDuration());
-    //         // System.out.println("Title: " + current.song.title + ", Artist: " + current.song.artist + ", Duration: " + current.song.duration);
-    //         current = current.next;
-    //     }
-    // }
-
-    /*
+    /**
     * Returns the number of songs in the playlist
     */
     public int getTotalDuration() {
@@ -263,7 +209,7 @@ public class FullyFunctionalPlaylist {
         return totalDuration;
     }
     
-    /*
+    /**
      * Converts total song duration to a formatted form "H:MM:SS"
      */
     public String getFormattedTotalDuration() {
@@ -280,7 +226,7 @@ public class FullyFunctionalPlaylist {
     }
 
 
-    /*
+    /**
      * Play next song in the playlist using song title of current song
      * @param title Title of current Song object being played
      * @return the next Song object
@@ -302,7 +248,7 @@ public class FullyFunctionalPlaylist {
         return null; // no song found
     }
 
-    /*
+    /**
      * Play previous song in the playlist using song title of current song
      * @param title Title of current Song object being played
      * @return the previous Song object
@@ -322,18 +268,18 @@ public class FullyFunctionalPlaylist {
         return null; // no song found
     }
 
-    /*
+    /**
      * Toggles the continuous play mode
      */
     public void toggleRepeatAll() {
         repeatAll = !repeatAll;
     }
 
-    /*
-    * Shuffle Method
-    * Shuffles the playlist using the Fisher-Yates Algorithm.
-    * For more info on the algorithm, see: 
-    * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    /**
+     * Shuffle Method
+     * Shuffles the playlist using the Fisher-Yates Algorithm.
+     * For more info on the algorithm, see: 
+     * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
     */
     public void shuffle() {
         if (head == null && tail == null) { // empty playist
@@ -368,4 +314,28 @@ public class FullyFunctionalPlaylist {
             numNodes[j].song = temp;
         }
     }
+
+    // Additional method -- for fun
+
+    /**
+     * Searches for songs by title or artist.
+     * @param keyword The title or artist to search for.
+     * @return A list of songs that match the keyword.
+     */
+    public List<Song> searchSongs(String keyword) {
+        List<Song> matchingSongs = new ArrayList<>();
+        if (head == null) {
+            return matchingSongs; // Return empty list if the playlist is empty
+        }
+        Node current = head;
+        do {
+            if (current.song.getTitle().equalsIgnoreCase(keyword) || current.song.getArtist().equalsIgnoreCase(keyword)) {
+                matchingSongs.add(current.song);
+            }
+            current = current.next;
+        } while (current != head); // Traverse the entire list
+
+        return matchingSongs;
+    }
+
 }
